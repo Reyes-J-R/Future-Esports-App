c73d939452c781f19c5b854beba9e68fe6ac50e6
Reyes JR <reyesjoeroshan@gmail.com>
2025-05-14 23:22:58 +0530

    configured backend connection

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // const API_BASE_URL = "http://localhost:8000"
-const API_BASE_URL = "https://6796-2001-df0-b240-414-19c0-11ba-d9c8-d560.ngrok-free.app"
+const API_BASE_URL = "https://reyesjr.pythonanywhere.com"
 // const API_BASE_URL = "https://fesports-backend.onrender.com"
 export default API_BASE_URL
